6c4784dc7df3a7c7a8394f84e27a4b3d5f05293c
OPoppys <163558253+OPoppys@users.noreply.github.com>
2024-05-20 23:50:36 -0300

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+nome = "Carlos Pilla"
+age = 18
+dm = "Grau II"
+dindin = 0.11
+
+print("Meu nome é:" , nome)
+print("Minha idade é:", age)
+print("Eu sou", dm)
+print("Tenho" ,dindin, "na minha conta")
